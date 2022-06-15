805338e8768ec579ab10e13fc5486f679707660c
Rui Ma <marui1@ruijie.com.cn>
2022-06-15 13:22:28 +0800

    增加默认脚本

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,7 +5,13 @@ commitId=$3
 cd /tmp
 git clone $ssh_url
 cd $name
-docker build -t $name:$commitId -f release/docker/Dockerfile .
+
+BUILD_FILE=release/docker/build.sh
+if [ -f "$BUILD_FILE" ]; then
+  /bin/bash $BUILD_FILE
+else
+  docker build -t $name:$commitId -f release/docker/Dockerfile .
+fi
 
 cd -
 rm -rf /tmp/$name
