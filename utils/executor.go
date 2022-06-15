ec0496c601974190b821b65de9f02167d75d59ad
Rui Ma <marui1@ruijie.com.cn>
2022-06-15 08:45:02 +0800

    新增脚本执行器

--- utils/executor.go
+++ utils/executor.go
@@ -49,3 +49,43 @@ func RunCommand(command string) []string {
 
 	return res
 }
+
+func RunScript(command string) []string {
+	res := make([]string, 0)
+	enc := mahonia.NewDecoder("gbk")
+
+	optSys := runtime.GOOS
+	cmd := exec.Command("bash", command)
+	if optSys == "windows" {
+		panic("仅支持在linux中执行脚本")
+	}
+	stdout, _ := cmd.StdoutPipe()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		reader := bufio.NewReader(stdout)
+		for {
+			readString, err := reader.ReadString('\n')
+			if err != nil || err == io.EOF {
+				return
+			}
+			fmt.Print(enc.ConvertString(readString))
+			res = append(res, enc.ConvertString(readString))
+		}
+	}()
+
+	err := cmd.Start()
+	wg.Wait()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Process PID: ", cmd.Process.Pid)
+	err = cmd.Wait()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return res
+}
