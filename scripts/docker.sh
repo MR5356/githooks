b28170effd9130aef8aef859ded0d6d0562f05fe
Rui Ma <marui1@ruijie.com.cn>
2022-06-17 09:48:28 +0800

    test

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,26 +2,26 @@ name=$1
 ssh_url=$2
 commitId=$3
 
-echo "$PWD/docker.sh clone project start"
+echo "scripts/docker.sh clone project start"
 cd /tmp
 git clone $ssh_url
 cd $name
-echo "$PWD/docker.sh clone project finish"
+echo "scripts/docker.sh clone project finish"
 
-echo "$PWD/docker.sh build image start"
+echo "scripts/docker.sh build image start"
 BUILD_FILE=release/docker/build.sh
 if [ -f "$BUILD_FILE" ]; then
   /bin/bash $BUILD_FILE $commitId
 else
   docker build -t $name:$commitId -f release/docker/Dockerfile .
 fi
-echo "$PWD/docker.sh build image finish"
+echo "scripts/docker.sh build image finish"
 
-echo "$PWD/docker.sh clean images start"
+echo "scripts/docker.sh clean images start"
 docker rmi `docker images|grep none|awk '{print $3 }'|xargs`
-echo "$PWD/docker.sh clean images finish"
+echo "scripts/docker.sh clean images finish"
 
 cd -
 rm -rf /tmp/$name
 
-echo "$PWD/docker.sh finish"
+echo "scripts/docker.sh finish"
