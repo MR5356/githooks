018cac45302cf92d083dc23914c76d2d0bb886c6
Rui Ma <marui1@ruijie.com.cn>
2022-06-17 10:11:03 +0800

    跳过没有Dockerfile的项目

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -10,12 +10,18 @@ echo "scripts/docker.sh clone project finish"
 
 echo "scripts/docker.sh build image start"
 BUILD_FILE=release/docker/build.sh
-if [ -f "$BUILD_FILE" ]; then
-  /bin/bash $BUILD_FILE $commitId
+Dockerfile=release/docker/Dockerfile
+
+if [ -f "$Dockerfile" ]; then
+  if [ -f "$BUILD_FILE" ]; then
+    /bin/bash $BUILD_FILE $commitId
+  else
+    docker build -t $name:$commitId -f release/docker/Dockerfile .
+  fi
+  echo "scripts/docker.sh build image finish"
 else
-  docker build -t $name:$commitId -f release/docker/Dockerfile .
+  echo "scripts/docker.sh build image skip"
 fi
-echo "scripts/docker.sh build image finish"
 
 echo "scripts/docker.sh clean images start"
 docker rmi `docker images|grep none|awk '{print $3 }'|xargs`
